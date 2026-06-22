c12e020c2d65f0e945a8bfccb8076b802c990a1f
Julio Collazos <amarullazo626@gmail.com>
2026-06-23 07:40:31 +1000

    add reference to the ambiorix application in the request object

--- R/ambiorix.R
+++ R/ambiorix.R
@@ -113,6 +113,7 @@ Ambiorix <- R6::R6Class(
         app = list(
           call = function(req) {
             request <- Request$new(req)
+            request$app <- self
             res <- Response$new(request)
             super$handle(request, res, routing::finalHandler(request, res))
           },
